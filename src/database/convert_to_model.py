f3ff2a34ae2a06d9ac05de32332ad63731dd454d
Pryann <pryann@gmail.com>
2024-06-11 22:20:18 +0200

    add auth decorator

--- src/database/convert_to_model.py
+++
@@ -1,6 +0,0 @@
-from typing import Type
-from pydantic import BaseModel
-
-
-def convert_to_model(obj: BaseModel, schema_type: Type[BaseModel]) -> BaseModel | None:
-    return schema_type.model_config(obj) if obj else None
